c8e25463269cdc8302e6a833a799e7556a449058
jlv <jing.lv@tcl.com>
2014-10-21 18:00:05 +0800

    update all pages of setting

--- SmartLinkV3/src/com/alcatel/smartlinkv3/ui/activity/SettingAboutActivity.java
+++ SmartLinkV3/src/com/alcatel/smartlinkv3/ui/activity/SettingAboutActivity.java
@@ -3,13 +3,17 @@ package com.alcatel.smartlinkv3.ui.activity;
 import com.alcatel.smartlinkv3.R;
 
 import android.app.Activity;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Window;
 import android.widget.TextView;
 
 public class SettingAboutActivity extends Activity {
 
 	private TextView m_tv_title = null;
+	private TextView m_tv_official;
+	private TextView m_tv_management;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -30,6 +34,15 @@ public class SettingAboutActivity extends Activity {
 	}
 	
 	private void createControls(){
-		
+		m_tv_official = (TextView)findViewById(R.id.tv_link_official);
+		m_tv_management = (TextView)findViewById(R.id.tv_link_management_page);
+//		m_tv_official.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+//		m_tv_management.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+		String strOfficial = getString(R.string.setting_about_official_website);
+		String strHtmlOfficial = "<u>"+strOfficial+"</u>";
+		m_tv_official.setText(Html.fromHtml(strHtmlOfficial));
+		String strManagement = getString(R.string.setting_about_management_page);
+		String strHtmlManagement = "<u>"+strManagement+"</u>";
+		m_tv_management.setText(Html.fromHtml(strHtmlManagement));
 	}
 }
--- SmartLinkV3/src/com/alcatel/smartlinkv3/ui/activity/SettingWifiActivity.java
+++ SmartLinkV3/src/com/alcatel/smartlinkv3/ui/activity/SettingWifiActivity.java
@@ -4,10 +4,19 @@ import com.alcatel.smartlinkv3.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View.OnClickListener;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
-public class SettingWifiActivity extends Activity {
+public class SettingWifiActivity extends Activity implements OnClickListener{
 
+	private EditText m_et_password;
+	private ImageButton m_ib_show_password;
+	private ImageButton m_ib_hide_password;
+	private ImageButton m_ib_back;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -16,6 +25,54 @@ public class SettingWifiActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.activity_setting_wifi);
 		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title_1);
+		//control title bar
+		controlTitlebar();
+		//create controls
+		createControls();
 	}
 
+	private void controlTitlebar(){
+		m_ib_back = (ImageButton)findViewById(R.id.ib_title_back);
+		m_ib_back.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				SettingWifiActivity.this.finish();
+			}
+		});
+	}
+	
+	private void createControls(){
+		m_et_password = (EditText)findViewById(R.id.edit_password);
+		m_ib_show_password = (ImageButton)findViewById(R.id.ib_show_password);
+		m_ib_hide_password = (ImageButton)findViewById(R.id.ib_hide_password);
+		if(!m_ib_show_password.isShown()){
+			m_et_password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+		}else {
+			m_et_password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+		}
+		
+		m_ib_hide_password.setOnClickListener(this);
+		m_ib_show_password.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		int nbtnID = v.getId();
+		switch(nbtnID){
+		case R.id.ib_hide_password:
+			m_ib_show_password.setVisibility(View.VISIBLE);
+			m_ib_hide_password.setVisibility(View.GONE);
+			m_et_password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+			break;
+		case R.id.ib_show_password:
+			m_ib_hide_password.setVisibility(View.VISIBLE);
+			m_ib_show_password.setVisibility(View.GONE);
+			m_et_password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+			break;
+		
+		}
+	}
 }
