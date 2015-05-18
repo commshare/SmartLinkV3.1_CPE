a181dfbdc77866ab40b730a493f50f10042dc91d
yqtian <yiqing.tian@tcl.com>
2015-05-18 10:26:29 +0800

    Update build script for SmartLinkV3.1

--- SmartLinkV3/build.sh
+++ SmartLinkV3/build.sh
@@ -1,7 +1,7 @@
-android update project -p . -n SmartLinkV3 -t android-16
+android update project -p . -n SmartLinkV3.1 -t android-16
 rm -rf ./gen
 rm -rf ./bin
 ant release
-mv ./bin/SmartLinkV3-release.apk "./bin/SmartLinkV3.apk"
+mv ./bin/SmartLinkV3.1-release.apk "./bin/SmartLinkV3.1.apk"
 
 
