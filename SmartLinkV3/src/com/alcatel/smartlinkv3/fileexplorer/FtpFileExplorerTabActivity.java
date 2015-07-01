d56b1c3958250430c29c9ec9692ecc849a8eea2a
xiaohua.lan <xiaohua.lan@tcl.com>
2015-07-01 15:26:31 +0800

    Add android-support-v7-appcompat.jar

--- SmartLinkV3/src/com/alcatel/smartlinkv3/fileexplorer/FtpFileExplorerTabActivity.java
+++ SmartLinkV3/src/com/alcatel/smartlinkv3/fileexplorer/FtpFileExplorerTabActivity.java
@@ -28,6 +28,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+//import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
