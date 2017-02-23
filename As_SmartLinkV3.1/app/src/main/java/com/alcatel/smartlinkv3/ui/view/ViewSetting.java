7f570818362f9dcca96caad6b331b7a687ffa85b
haodi.liang <haodi.liang@jrdcom.com>
2017-02-23 19:32:10 +0800

    【2017年2月23日19:31:13】精简代码，settings中完成显示固件版本号。

--- As_SmartLinkV3.1/app/src/main/java/com/alcatel/smartlinkv3/ui/view/ViewSetting.java
+++ As_SmartLinkV3.1/app/src/main/java/com/alcatel/smartlinkv3/ui/view/ViewSetting.java
@@ -8,12 +8,7 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alcatel.smartlinkv3.R;
@@ -38,28 +33,9 @@ import java.util.List;
 
 public class ViewSetting extends BaseViewImpl implements View.OnClickListener {
 
-
-    private final int ITEM_ACCOUNT_SETTING1 = 0;
-    private final int ITEM_NETWORK_SETTING1 = 1;
-    private final int ITEM_DEVICE_SETTING1  = 2;
-    //	private final int ITEM_PRINTER_SETTING1 =3;
-    //	private final int ITEM_USB_SETTING1 =4;
-    private final int ITEM_ABOUT_SETTING1   = 3;
-
-    private final int ITEM_ACCOUNT_SETTING2 = 0;
-    private final int ITEM_NETWORK_SETTING2 = 1;
-    private final int ITEM_SHARE_SETTING    = 2;
-    private final int ITEM_DEVICE_SETTING2  = 3;
-    //    private final int ITEM_PRINTER_SETTING2 =4;
-    //    private final int ITEM_USB_SETTING2 =5;
-    private final int ITEM_ABOUT_SETTING2   = 4;
-
     boolean isFtpSupported  = false;
     boolean isDlnaSupported = false;
     private boolean  isSharingSupported  = false;
-    private ListView m_lvSettingListView = null;
-    private UprgadeAdapter    adapter;
-    private List<SettingItem> list;
     private boolean m_blFirst = true;
     private settingBroadcast m_receiver;
     private int              upgradeStatus;
@@ -93,93 +69,6 @@ public class ViewSetting extends BaseViewImpl implements View.OnClickListener {
     }
 
     private void initUI() {
-        //		m_lvSettingListView.setAdapter(adapter);
-        //		if(isSharingSupported){
-        //			m_lvSettingListView.setOnItemClickListener(new OnItemClickListener() {
-        //
-        //				@Override
-        //				public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-        //						long arg3) {
-        //					// TODO Auto-generated method stub
-        //					switch(arg2){
-        //					case ITEM_ACCOUNT_SETTING2:
-        //                        goToAccountSettingPage();
-        //						break;
-        //					case ITEM_NETWORK_SETTING2:
-        //                        goToNetworkSettingPage();
-        //						break;
-        //					case ITEM_SHARE_SETTING:
-        //                        goToShareSettingPage();
-        //						break;
-        //					case ITEM_DEVICE_SETTING2:
-        //                        goToDeviceSettingPage();
-        //						break;
-        ////					case ITEM_PRINTER_SETTING2:
-        ////                        goToAboutSettingPage();
-        ////						break;
-        ////					case ITEM_USB_SETTING2:
-        ////						goToAboutSettingPage();
-        ////						break;
-        //					case ITEM_ABOUT_SETTING2:
-        //						goToAboutSettingPage();
-        //						break;
-        ////					case ITEM_POWER_SETTING:
-        ////						goToPowerSettingPage();
-        ////						break;
-        ////					case ITEM_BACKUP_SETTING:
-        ////						goToBackupSettingPage();
-        ////						break;
-        ////					case ITEM_UPGRADE_SETTING:
-        ////						goToUpgradeSettingPage();
-        ////						break;
-        //					default:
-        //						break;
-        //					}
-        //				}
-        //			});
-        //		}
-        //		else{
-        //			m_lvSettingListView.setOnItemClickListener(new OnItemClickListener() {
-        //
-        //				@Override
-        //				public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-        //						long arg3) {
-        //					// TODO Auto-generated method stub
-        //					switch(arg2){
-        //                        case ITEM_ACCOUNT_SETTING1:
-        //                            goToAccountSettingPage();
-        //                            break;
-        //                        case ITEM_NETWORK_SETTING1:
-        //                            goToNetworkSettingPage();
-        //                            break;
-        //                        case ITEM_DEVICE_SETTING1:
-        //                            goToDeviceSettingPage();
-        //                            break;
-        ////                        case ITEM_PRINTER_SETTING1:
-        ////                            goToAboutSettingPage();
-        ////                            break;
-        ////                        case ITEM_USB_SETTING1:
-        ////                            goToAboutSettingPage();
-        ////                            break;
-        //                        case ITEM_ABOUT_SETTING1:
-        //                            goToAboutSettingPage();
-        //                            break;
-        ////					case ITEM_POWER_SETTING:
-        ////						goToPowerSettingPage();
-        ////						break;
-        ////					case ITEM_BACKUP_SETTING:
-        ////						goToBackupSettingPage();
-        ////						break;
-        ////					case ITEM_UPGRADE_SETTING:
-        ////						goToUpgradeSettingPage();
-        ////						break;
-        //					default:
-        //						break;
-        //					}
-        //				}
-        //			});
-        //		}
-
         mLoginPassword.setOnClickListener(this);
         mMobileNetwork.setOnClickListener(this);
         mEthernetWan.setOnClickListener(this);
@@ -194,7 +83,6 @@ public class ViewSetting extends BaseViewImpl implements View.OnClickListener {
     protected void init() {
         m_view = LayoutInflater.from(m_context).inflate(R.layout.view_setting,
                 null);
-        //		m_lvSettingListView = (ListView)m_view.findViewById(R.id.settingList);
         mLoginPassword = (RelativeLayout) m_view.findViewById(R.id.setting_login_password);
         mMobileNetwork = (RelativeLayout) m_view.findViewById(R.id.setting_mobile_network);
         mEthernetWan = (RelativeLayout) m_view.findViewById(R.id.setting_ethernet_wan);
@@ -203,9 +91,6 @@ public class ViewSetting extends BaseViewImpl implements View.OnClickListener {
         mRestart = (RelativeLayout) m_view.findViewById(R.id.setting_restart);
         mBackup = (RelativeLayout) m_view.findViewById(R.id.setting_backup);
         mAbout = (RelativeLayout) m_view.findViewById(R.id.setting_about);
-
-        //        list = getData(m_context);
-        //        adapter = new UprgadeAdapter(m_context, list);
     }
 
     @Override
@@ -221,20 +106,9 @@ public class ViewSetting extends BaseViewImpl implements View.OnClickListener {
         if (EnumDeviceCheckingStatus.DEVICE_NEW_VERSION == EnumDeviceCheckingStatus.build(nUpgradeStatus)) {
             //DEVICE_NO_NEW_VERSION
             m_blFirst = false;
-            if (isSharingSupported) {
-                changeUpgradeFlag(ITEM_DEVICE_SETTING2, true);
-            } else {
-                changeUpgradeFlag(ITEM_DEVICE_SETTING1, true);
-            }
             //保存升级标志到本地SP
             SharedPrefsUtil.getInstance(m_context).putBoolean(ISDEVICENEWVERSION, true);
         } else {
-            if (isSharingSupported) {
-                changeUpgradeFlag(ITEM_DEVICE_SETTING2, false);
-            } else {
-                changeUpgradeFlag(ITEM_DEVICE_SETTING1, false);
-            }
-
             //保存升级标志到本地SP
             SharedPrefsUtil.getInstance(m_context).putBoolean(ISDEVICENEWVERSION, false);
         }
@@ -280,12 +154,6 @@ public class ViewSetting extends BaseViewImpl implements View.OnClickListener {
             //保存升级标志到本地SP
             SharedPrefsUtil.getInstance(context).putBoolean(ISDEVICENEWVERSION, false);
         }
-
-        //		item = new SettingItem(context.getString(R.string.setting_printer), false);
-        //		list.add(item);
-        //
-        //		item = new SettingItem(context.getString(R.string.setting_usb_storage), false);
-        //		list.add(item);
 
         item = new SettingItem(context.getString(R.string.setting_about), false);
         list.add(item);
@@ -384,105 +252,6 @@ public class ViewSetting extends BaseViewImpl implements View.OnClickListener {
         }
     }
 
-    public class UprgadeAdapter extends BaseAdapter {
-
-        private Context           context;
-        private List<SettingItem> listSettingItmes;
-
-        public UprgadeAdapter(Context context, List<SettingItem> list) {
-            this.context = context;
-            this.listSettingItmes = list;
-        }
-
-        @Override
-        public int getCount() {
-            // TODO Auto-generated method stub
-            return listSettingItmes.size();
-        }
-
-        @Override
-        public Object getItem(int arg0) {
-            // TODO Auto-generated method stub
-            return listSettingItmes.get(arg0);
-        }
-
-        @Override
-        public long getItemId(int arg0) {
-            // TODO Auto-generated method stub
-            return arg0;
-        }
-
-        @Override
-        public View getView(int arg0, View convertView, ViewGroup arg2) {
-            // TODO Auto-generated method stub
-            View itemView = LayoutInflater.from(this.context).inflate(R.layout.setting_item,
-                    null);
-            //
-            SettingItem item = listSettingItmes.get(arg0);
-            ImageView ItemIconIv = (ImageView) itemView.findViewById(R.id.settingItemIcon);
-            TextView tvItemNameTextView = (TextView) itemView.findViewById(R.id.settingItemText);
-            TextView ivUpgrade = (TextView) itemView.findViewById(R.id.flagUpgrade);
-            tvItemNameTextView.setText(item.getItemName());
-
-            //图标处理
-            if (isSharingSupported) {
-                switch (arg0) {
-                    case 0:
-                        ItemIconIv.setImageResource(R.drawable.settings_ic_account);
-                        break;
-                    case 1:
-                        ItemIconIv.setImageResource(R.drawable.settings_ic_network);
-                        break;
-                    case 2:
-                        ItemIconIv.setImageResource(R.drawable.settings_ic_sharing);
-                        break;
-                    case 3:
-                        ItemIconIv.setImageResource(R.drawable.settings_ic_device);
-                        break;
-                    //                    case 4:
-                    //                        ItemIconIv.setImageResource(R.drawable.settings_ic_printer);
-                    //                        break;
-                    //                    case 5:
-                    //                        ItemIconIv.setImageResource(R.drawable.settings_ic_usb);
-                    //                        break;
-                    case 4:
-                        ItemIconIv.setImageResource(R.drawable.settings_ic_about);
-                        break;
-                }
-            } else {
-                switch (arg0) {
-                    case 0:
-                        ItemIconIv.setImageResource(R.drawable.settings_ic_account);
-                        break;
-                    case 1:
-                        ItemIconIv.setImageResource(R.drawable.settings_ic_network);
-                        break;
-                    case 2:
-                        ItemIconIv.setImageResource(R.drawable.settings_ic_device);
-                        break;
-                    //                    case 3:
-                    //                        ItemIconIv.setImageResource(R.drawable.settings_ic_printer);
-                    //                        break;
-                    //                    case 4:
-                    //                        ItemIconIv.setImageResource(R.drawable.settings_ic_usb);
-                    //                        break;
-                    case 3:
-                        ItemIconIv.setImageResource(R.drawable.settings_ic_about);
-                        break;
-                }
-            }
-
-            Boolean blUpgrade = item.getUpgradeFlag();
-            if (blUpgrade) {
-                ivUpgrade.setVisibility(View.VISIBLE);
-            } else {
-                ivUpgrade.setVisibility(View.GONE);
-            }
-            return itemView;
-        }
-
-    }
-
     public class SettingItem {
         //Item name
         private String  m_strItem;
@@ -504,23 +273,6 @@ public class ViewSetting extends BaseViewImpl implements View.OnClickListener {
 
         public String getItemName() {
             return m_strItem;
-        }
-    }
-
-    private void changeUpgradeFlag(int itemIndex, Boolean blUpgrade) {
-        if (itemIndex >= ITEM_ACCOUNT_SETTING2 && itemIndex <= ITEM_ABOUT_SETTING2) {
-            SettingItem item = list.get(itemIndex);
-            Boolean blOldUpgrade = item.getUpgradeFlag();
-            if (blOldUpgrade != blUpgrade) {
-                item.setUpgradeFlag(blUpgrade);
-                ArrayList<SettingItem> settingItems = new ArrayList<SettingItem>();
-                for (int i = 0; i < list.size(); i++) {
-                    settingItems.add(i, list.get(i));
-                }
-                list.clear();
-                list.addAll(settingItems);
-                adapter.notifyDataSetChanged();
-            }
         }
     }
 
@@ -548,19 +300,19 @@ public class ViewSetting extends BaseViewImpl implements View.OnClickListener {
                         //EnumDeviceCheckingStatus.DEVICE_NEW_VERSION
                         //EnumDeviceCheckingStatus.DEVICE_NO_NEW_VERSION
                         m_blFirst = false;
-                        if (isSharingSupported) {
-                            changeUpgradeFlag(ITEM_DEVICE_SETTING2, true);
-                        } else {
-                            changeUpgradeFlag(ITEM_DEVICE_SETTING1, true);
-                        }
+//                        if (isSharingSupported) {
+//                            changeUpgradeFlag(ITEM_DEVICE_SETTING2, true);
+//                        } else {
+//                            changeUpgradeFlag(ITEM_DEVICE_SETTING1, true);
+//                        }
                         //保存升级标志到本地SP
                         SharedPrefsUtil.getInstance(context).putBoolean(ISDEVICENEWVERSION, true);
                     } else {
-                        if (isSharingSupported) {
-                            changeUpgradeFlag(ITEM_DEVICE_SETTING2, false);
-                        } else {
-                            changeUpgradeFlag(ITEM_DEVICE_SETTING1, false);
-                        }
+//                        if (isSharingSupported) {
+//                            changeUpgradeFlag(ITEM_DEVICE_SETTING2, false);
+//                        } else {
+//                            changeUpgradeFlag(ITEM_DEVICE_SETTING1, false);
+//                        }
                         //保存升级标志到本地SP
                         SharedPrefsUtil.getInstance(context).putBoolean(ISDEVICENEWVERSION, false);
                     }
