c35050e322ef7dba8687a87e26e51a4083811925
Shenlong Xu <shenlong.xu.sz@tcl.com>
2017-03-17 15:21:28 +0800

    delete folder As_SmartLinkV3

--- As_SmartLinkV3/app/src/main/java/com/alcatel/smartlinkv3/samba/SmbError.java
+++
@@ -1,8 +0,0 @@
-package com.alcatel.smartlinkv3.samba;
-
-public class SmbError {	
-	public static final int ERROR_RES = -1;	
-	
-	public static final int  SMB_ERR_LOGON_FAILURE = 0xC000006D;	
-	public static final int  SMB_ERR_DISK_FULL = 0xC000007F;	
-}
--- As_SmartLinkV3/app/src/main/java/com/alcatel/smartlinkv3/ui/view/ViewIndex.java
+++
@@ -1,9 +0,0 @@
-package com.alcatel.smartlinkv3.ui.view;
-
-public class ViewIndex {
-	//
-	public static final int VIEW_HOME = 0;
-	public static final int VIEW_USAGE = 1;
-	public static final int VIEW_SMS = 2;
-	public static final int VIEW_SETTINGE = 3;
-}
