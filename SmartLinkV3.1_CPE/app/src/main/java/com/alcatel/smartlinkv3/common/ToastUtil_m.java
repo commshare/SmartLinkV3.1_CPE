2692e2a35b9e994558840c0f7cdce37998452686
qianli.ma <qianli.ma@jrdcom.com>
2017-06-05 13:54:47 +0800

    1.ADD function of WAN data handle

---
+++ SmartLinkV3.1_CPE/app/src/main/java/com/alcatel/smartlinkv3/common/ToastUtil_m.java
@@ -0,0 +1,39 @@
+package com.alcatel.smartlinkv3.common;
+
+import android.app.Activity;
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtil_m {
+    public static void show(Context context, final String tip) {
+        String threadName = Thread.currentThread().getName();
+        if (threadName.equalsIgnoreCase("main")) {
+            Toast.makeText(context, tip, Toast.LENGTH_SHORT).show();
+        } else {
+            final Activity activity = (Activity) context;
+            activity.runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    Toast.makeText(activity, tip, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    public static void showLong(Context context, final String tip) {
+        String threadName = Thread.currentThread().getName();
+        if (threadName.equalsIgnoreCase("main")) {
+            Toast.makeText(context, tip, Toast.LENGTH_LONG).show();
+        } else {
+            final Activity activity = (Activity) context;
+            activity.runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    Toast.makeText(activity, tip, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
