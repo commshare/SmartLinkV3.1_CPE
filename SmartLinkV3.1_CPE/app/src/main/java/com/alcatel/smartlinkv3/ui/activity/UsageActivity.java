bec7e6b6de714b795b66429ef2619c5e4621e656
yaodong.zhang <yaodong.zhang@jrdcom.com>
2017-06-29 15:04:32 +0800

    完成UsageActivity

--- SmartLinkV3.1_CPE/app/src/main/java/com/alcatel/smartlinkv3/ui/activity/UsageActivity.java
+++ SmartLinkV3.1_CPE/app/src/main/java/com/alcatel/smartlinkv3/ui/activity/UsageActivity.java
@@ -1,105 +1,54 @@
 package com.alcatel.smartlinkv3.ui.activity;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.alcatel.smartlinkv3.R;
 import com.alcatel.smartlinkv3.common.CommonUtil;
-import com.alcatel.smartlinkv3.common.MessageUti;
-import com.alcatel.smartlinkv3.httpservice.BaseResponse;
 import com.alcatel.smartlinkv3.model.Usage.UsageRecord;
-import com.alcatel.smartlinkv3.model.Usage.UsageSetting;
 import com.alcatel.smartlinkv3.network.API;
 import com.alcatel.smartlinkv3.network.MySubscriber;
-import com.alcatel.smartlinkv3.ui.dialog.MorePopWindow;
 import com.alcatel.smartlinkv3.ui.home.helper.main.TimerHelper;
 import com.alcatel.smartlinkv3.utils.ActionbarSetting;
 import com.alcatel.smartlinkv3.utils.DataUtils;
+
+import static com.alcatel.smartlinkv3.R.id.tv_network_settings;
 
 
 public class UsageActivity extends BaseActivityWithBack implements View.OnClickListener {
 
     private Context mContext;
-
-    /*home_panel  start*/
-    private ImageView m_homewarn;
-    private TextView m_homedata;
-    private TextView m_homedataprogressdec;
-
-    private ProgressBar m_homedataprogress;
-    private TextView m_homedowndata;
-    private TextView m_homeupdata;
-                /*home_panel  end*/
-
-    /*roaming_panel  start*/
-    private ImageView m_roamingwarn;
-    private TextView m_roamingdata;
-    private TextView m_roamingdowndata;
-    private TextView m_roamingupdata;
-                /*roaming_panel  end*/
-
-    /*duration_panel  start*/
-    private ImageView m_durationwarn;
-    private TextView m_durationtotaltime;
-    private TextView m_durationtime;
-                /*duration_panel  end*/
+    private TextView mHomeData;
+    private TextView mTvHomeTime;
+    private TextView mRoamingData;
+    private TextView mTvRoamingTime;
+    private Button mBtReset;
+    private TextView mTvNetworkSettings;
 
     private ImageButton mbackBtn;
     private Button mMoreBtn;
     private TimerHelper timerHelper;
-    private int timeLimitTimes = 0;
-    int nProgress;
+    private boolean mIsLimited;
 
     public UsageActivity() {
         this.mContext = this;
-    }
-
-    private class ViewUsageBroadcastReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            BaseResponse response = intent.getParcelableExtra(MessageUti.HTTP_RESPONSE);
-            Boolean ok = response != null && response.isOk();
-            if (intent.getAction().equals(MessageUti.STATISTICS_GET_USAGE_HISTORY_ROLL_REQUSET)) {
-                if (ok) {
-                    updateUI();
-                }
-            } else if (intent.getAction().equals(MessageUti.STATISTICS_CLEAR_ALL_RECORDS_REQUSET)) {
-                String msgRes;
-                if (ok) {
-                    msgRes = getString(R.string.usage_clear_history_success);
-                    Toast.makeText(mContext, msgRes, Toast.LENGTH_SHORT).show();
-                } else {
-                    msgRes = mContext.getString(R.string.usage_clear_history_fail);
-                    Toast.makeText(mContext, msgRes, Toast.LENGTH_SHORT).show();
-                }
-            } else if (intent.getAction().equals(MessageUti.STATISTICS_GET_USAGE_SETTINGS_ROLL_REQUSET)) {
-                if (ok) {
-                    updateUI();
-                }
-            }
-        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_usage);
-
         // init action bar
         initActionBar();
         // init layout ui
         initView();
-
         // init status
         updateUI();
         // init Timer
@@ -131,27 +80,20 @@ public class UsageActivity extends BaseActivityWithBack implements View.OnClickL
                 mbackBtn = (ImageButton) view.findViewById(R.id.usage_back);
                 mMoreBtn = (Button) view.findViewById(R.id.usage_more);
                 mbackBtn.setOnClickListener(UsageActivity.this);
-                mMoreBtn.setOnClickListener(UsageActivity.this);
+                mMoreBtn.setVisibility(View.GONE);
             }
         }.settingActionbarAttr(this, getSupportActionBar(), R.layout.actionbarusage);
     }
 
     protected void initView() {
-        m_homewarn = (ImageView) findViewById(R.id.home_warn);
-        m_homedata = (TextView) findViewById(R.id.home_data);
-        m_homedataprogress = (ProgressBar) findViewById(R.id.usage_status_progress);
-        m_homedowndata = (TextView) findViewById(R.id.home_data_down_status);
-        m_homeupdata = (TextView) findViewById(R.id.home_data_up_status);
-        m_homedataprogressdec = (TextView) findViewById(R.id.usage_status_describtion);
-        m_roamingwarn = (ImageView) findViewById(R.id.roaming_warn);
-        m_roamingdata = (TextView) findViewById(R.id.roaming_data);
-        m_roamingdowndata = (TextView) findViewById(R.id.roaming_data_down_status);
-        m_roamingupdata = (TextView) findViewById(R.id.roaming_data_up_status);
-
-        m_durationwarn = (ImageView) findViewById(R.id.duration_warn);
-        m_durationtotaltime = (TextView) findViewById(R.id.duration_total_time);
-        m_durationtime = (TextView) findViewById(R.id.duration_time_status);
-
+        mHomeData = (TextView) findViewById(R.id.home_data);
+        mTvHomeTime = (TextView) findViewById(R.id.tv_home_time);
+        mRoamingData = (TextView) findViewById(R.id.roaming_data);
+        mTvRoamingTime = (TextView) findViewById(R.id.tv_roaming_time);
+        mBtReset = (Button) findViewById(R.id.bt_reset);
+        mTvNetworkSettings = (TextView) findViewById(tv_network_settings);
+        mBtReset.setOnClickListener(this);
+        mTvNetworkSettings.setOnClickListener(this);
     }
 
     @Override
@@ -160,21 +102,40 @@ public class UsageActivity extends BaseActivityWithBack implements View.OnClickL
             case R.id.usage_back:
                 finish();
                 break;
-            case R.id.usage_more:
-                moreBtnClick();
+            case R.id.bt_reset:
+                resetDataUsage();
+                break;
+            case tv_network_settings:
+                goToMobileNetworkSettingPage();
                 break;
             default:
                 break;
         }
     }
 
-    private void moreBtnClick() {
-        MorePopWindow morePopWindow = new MorePopWindow(this);
-        // when user click this button & request the clear action then refresh ui
-        morePopWindow.setOnClearUsageRecord(() -> updateUI());
-        // clear action
-        morePopWindow.showPopupWindow(mMoreBtn);
+    private void resetDataUsage() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle(R.string.reset_monthly_data_usage_statistics);
+        dialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.setPositiveButton(R.string.reset, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                API.get().setUsageRecordClear(DataUtils.getCurrent(), new MySubscriber() {
+                    @Override
+                    protected void onSuccess(Object result) {
+                        updateUI();
+                    }
+                });
+            }
+        });
+        dialog.show();
     }
+
 
     @Override
     protected void onResume() {
@@ -184,22 +145,13 @@ public class UsageActivity extends BaseActivityWithBack implements View.OnClickL
 
     /* update ui */
     private void updateUI() {
-        // get usage setting
-        getUsageSetting();
-
-        // get usage record
         getUsageRecord();
     }
 
-    /* **** getUsageSetting **** */
-    private void getUsageSetting() {
-        API.get().getUsageSetting(new MySubscriber<UsageSetting>() {
-            @Override
-            protected void onSuccess(UsageSetting result) {
-                // set ui by result
-                updateUsageSetting(result);
-            }
-        });
+    private void goToMobileNetworkSettingPage() {
+        // to setting network activity
+        Intent intent = new Intent(this, SettingNetworkActivity.class);
+        startActivity(intent);
     }
 
     /* **** getUsageRecord **** */
@@ -207,70 +159,39 @@ public class UsageActivity extends BaseActivityWithBack implements View.OnClickL
         API.get().getUsageRecord(DataUtils.getCurrent(), new MySubscriber<UsageRecord>() {
             @Override
             protected void onSuccess(UsageRecord result) {
+                int monthlyPlan = result.getMonthlyPlan();
+                if (monthlyPlan == 0) {
+                    mIsLimited = true;
+                } else {
+                    mIsLimited = false;
+                }
                 updateUsageRecord(result);
             }
         });
     }
 
-
-    /*  */
-    public void updateUsageSetting(UsageSetting result) {
-        timeLimitTimes = result.getTimeLimitTimes();
-        if (result.getMonthlyPlan() != 0) {
-            m_homedataprogressdec.setVisibility(View.GONE);
-            m_homedata.setVisibility(View.VISIBLE);
-        } else {
-            m_homedata.setVisibility(View.GONE);
-            m_homedataprogress.setProgress(0);
-            m_homedataprogressdec.setVisibility(View.VISIBLE);
-            m_homewarn.setVisibility(View.GONE);
-        }
-    }
-
+    //
     private void updateUsageRecord(UsageRecord result) {
         int useData = result.getHUseData();
-        m_homedata.setText(CommonUtil.ConvertTrafficToStringFromMB(mContext, useData) + " of " + CommonUtil.ConvertTrafficToStringFromMB(mContext, result.getMonthlyPlan()));
+        if (mIsLimited) {
+            mHomeData.setText(CommonUtil.ConvertTrafficToStringFromMB(mContext, useData) + " / " + CommonUtil.ConvertTrafficToStringFromMB(mContext, result.getMonthlyPlan()));
+
+        } else {
+            mHomeData.setText(CommonUtil.ConvertTrafficToStringFromMB(mContext, useData));
+
+        }
         if (result.getMonthlyPlan() <= 0) {// user have no set plan
             return;
         }
-        nProgress = useData * m_homedataprogress.getMax() / result.getMonthlyPlan();
-        if (nProgress > m_homedataprogress.getMax())
-            nProgress = m_homedataprogress.getMax();
-        m_homedataprogress.setProgress(nProgress);
-        if (useData > result.getMonthlyPlan()) {
-            m_homewarn.setVisibility(View.VISIBLE);
-        } else {
-            m_homewarn.setVisibility(View.GONE);
-        }
-        int traffic_HD = result.getHCurrUseDL();
-        int traffic_HU = result.getHCurrUseUL();
-        m_homedowndata.setText(CommonUtil.ConvertTrafficToStringFromMB(mContext, (long) traffic_HD));
-        m_homeupdata.setText(CommonUtil.ConvertTrafficToStringFromMB(mContext, (long) traffic_HU));
-
-
-        int traffic_RD = result.getRCurrUseDL();
-        int traffic_RU = result.getRCurrUseUL();
-        int RoamUseData = result.getRoamUseData();
-        m_roamingdowndata.setText(CommonUtil.ConvertTrafficToStringFromMB(mContext, (long) traffic_RD));
-        m_roamingupdata.setText(CommonUtil.ConvertTrafficToStringFromMB(mContext, (long) traffic_RU));
-        m_roamingdata.setText(CommonUtil.ConvertTrafficToStringFromMB(mContext, (long) RoamUseData));
-
-
+        mRoamingData.setText(CommonUtil.ConvertTrafficToStringFromMB(mContext, (long) result.getRoamUseData()));
         String durationformat = getResources().getString(R.string.usage_duration);
 
         int connectTime = result.getCurrConnTimes();
         int totalTime = result.getTConnTimes();
         String strCurrDuration = String.format(durationformat, connectTime / 3600, (connectTime % 3600) / 60);
-        m_durationtime.setText(strCurrDuration);
+        mTvHomeTime.setText(strCurrDuration);
         String strTotalDuration = String.format(durationformat, totalTime / 3600, (totalTime % 3600) / 60);
-        m_durationtotaltime.setText(strTotalDuration);
-
-        int HTimeLimitTimes = timeLimitTimes;
-        if (connectTime > (HTimeLimitTimes * 60)) {
-            m_durationwarn.setVisibility(View.VISIBLE);
-        } else {
-            m_durationwarn.setVisibility(View.GONE);
-        }
+        mTvRoamingTime.setText(strTotalDuration);
     }
 }
 
