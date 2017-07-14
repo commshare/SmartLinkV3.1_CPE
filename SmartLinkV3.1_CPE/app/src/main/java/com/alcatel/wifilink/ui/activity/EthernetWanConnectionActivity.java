a58019a762403e6ad298ac42730ba92259d7033b
jianqiang.li <jianqiang.li@jrdcom.com>
2017-07-14 16:25:57 +0800

    修改EthernetWanConnection的功能

--- SmartLinkV3.1_CPE/app/src/main/java/com/alcatel/wifilink/ui/activity/EthernetWanConnectionActivity.java
+++ SmartLinkV3.1_CPE/app/src/main/java/com/alcatel/wifilink/ui/activity/EthernetWanConnectionActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import com.alcatel.wifilink.R;
+import com.alcatel.wifilink.model.wan.WanSettingsParams;
 import com.alcatel.wifilink.model.wan.WanSettingsResult;
 import com.alcatel.wifilink.network.API;
 import com.alcatel.wifilink.network.MySubscriber;
@@ -37,13 +39,19 @@ public class EthernetWanConnectionActivity extends BaseActivityWithBack implemen
     private EditText mStaticIpSecondaryDns;
     private TextView mStaticIpMtu;
 
+    private Button mConnectOrDisconnect;
+    private WanSettingsResult mWanSettingsResult;
+    private WanSettingsParams mWanSettingsParams;
+    private boolean mIsConnecting;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ethernet_wan_connection);
-        setTitle("Ethernet WAN connection");
-
+        setTitle(getString(R.string.ethernet_wan_connection));
+        mWanSettingsResult = new WanSettingsResult();
+        mWanSettingsParams = new WanSettingsParams();
         mSelectedPppopImg = (ImageView) findViewById(R.id.pppoe_selected_img);
         mSelectedDhcpImg = (ImageView) findViewById(R.id.dhcp_selected_img);
         mSelectedStaticIpImg = (ImageView) findViewById(R.id.static_ip_selected_img);
@@ -67,6 +75,11 @@ public class EthernetWanConnectionActivity extends BaseActivityWithBack implemen
         mStaticIpPreferredDns = (EditText) findViewById(R.id.static_ip_preferred_dns);
         mStaticIpSecondaryDns = (EditText) findViewById(R.id.static_ip_secondary_dns);
         mStaticIpMtu = (TextView) findViewById(R.id.static_ip_mtu);
+
+        mConnectOrDisconnect = (Button) findViewById(R.id.btn_connect);
+        mConnectOrDisconnect.setOnClickListener(this);
+
+        mIsConnecting = false;
         getWanSettings();
     }
 
@@ -75,6 +88,20 @@ public class EthernetWanConnectionActivity extends BaseActivityWithBack implemen
             @Override
             protected void onSuccess(WanSettingsResult result) {
                 Log.v(TAG,"account"+result);
+                mWanSettingsResult = result;
+                if(result.getStatus() == 1 && mIsConnecting) {
+                    getWanSettings();
+                    return;
+                } else if(result.getStatus() == 2) {
+                    mIsConnecting = false;
+                }
+                if(result.getConnectType() == 0) {
+                    showConnectPppoe();
+                } else if(result.getConnectType() == 1) {
+                    showConnectDhcp();
+                } else if(result.getConnectType() == 2) {
+                    showConnectStaticIp();
+                }
                 mPppoeAccount.setText(result.getAccount().toString());
                 mPppoePassword.setText(result.getPassword().toString());
                 mPppoeMtu.setText(result.getPppoeMtu()+"");
@@ -92,38 +119,105 @@ public class EthernetWanConnectionActivity extends BaseActivityWithBack implemen
         });
     }
 
+    private void setWanSettings() {
+        API.get().setWanSettings(mWanSettingsParams, new MySubscriber() {
+            @Override
+            protected void onSuccess(Object result) {
+                Toast.makeText(EthernetWanConnectionActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                getWanSettings();
+            }
+            @Override
+            protected void onFailure() {
+                mIsConnecting = false;
+                Log.d(TAG, "setConnectionSettings error");
+            }
+        });
+    }
+
     @Override
     public void onClick(View v) {
         // TODO Auto-generated method stub
         int nID = v.getId();
         switch (nID) {
             case R.id.ethernet_wan_connection_pppoe:
-                mSelectedPppopImg.setVisibility(View.VISIBLE);
-                mSelectedDhcpImg.setVisibility(View.GONE);
-                mSelectedStaticIpImg.setVisibility(View.GONE);
-                mPppopLayout.setVisibility(View.VISIBLE);
-                mDhcpTextview.setVisibility(View.GONE);
-                mStaticIpLayout.setVisibility(View.GONE);
+                showConnectPppoe();
                 break;
             case R.id.ethernet_wan_connection_dhcp:
-                mSelectedPppopImg.setVisibility(View.GONE);
-                mSelectedDhcpImg.setVisibility(View.VISIBLE);
-                mSelectedStaticIpImg.setVisibility(View.GONE);
-                mPppopLayout.setVisibility(View.GONE);
-                mDhcpTextview.setVisibility(View.VISIBLE);
-                mStaticIpLayout.setVisibility(View.GONE);
+                showConnectDhcp();
                 break;
             case R.id.ethernet_wan_connection_static_ip:
-                mSelectedPppopImg.setVisibility(View.GONE);
-                mSelectedDhcpImg.setVisibility(View.GONE);
-                mSelectedStaticIpImg.setVisibility(View.VISIBLE);
-                mPppopLayout.setVisibility(View.GONE);
-                mDhcpTextview.setVisibility(View.GONE);
-                mStaticIpLayout.setVisibility(View.VISIBLE);
+                showConnectStaticIp();
+                break;
+            case R.id.btn_connect:
+                if(mConnectOrDisconnect.getText().toString().equals(getString(R.string.connect))) {
+                    connectWan();
+                } else {
+                    Toast.makeText(EthernetWanConnectionActivity.this, R.string.it_is_connected_you_can_switch_other_connection_mode_page, Toast.LENGTH_SHORT).show();
+                }
                 break;
             default:
                 break;
         }
+    }
+
+    private void connectWan() {
+        mWanSettingsParams.setAccount(mPppoeAccount.getText().toString().trim());
+        mWanSettingsParams.setPassword(mPppoePassword.getText().toString().trim());
+        mWanSettingsParams.setPppoeMtu(mWanSettingsResult.getPppoeMtu());
+        mWanSettingsParams.setStaticIpAddress(mStaticIpAddress.getText().toString().trim());
+        mWanSettingsParams.setIpAddress(mWanSettingsResult.getIpAddress());
+        mWanSettingsParams.setSubNetMask(mStaticIpSubnetMask.getText().toString().trim());
+        mWanSettingsParams.setGateway(mStaticIpDefaultGateway.getText().toString());
+        mWanSettingsParams.setPrimaryDNS(mStaticIpPreferredDns.getText().toString());
+        mWanSettingsParams.setSecondaryDNS(mStaticIpSecondaryDns.getText().toString());
+        mWanSettingsParams.setMtu(mWanSettingsResult.getMtu());
+        mIsConnecting = true;
+        setWanSettings();
+    }
+
+    private void showConnectStaticIp() {
+        if(mWanSettingsResult.getConnectType() == 2 && mWanSettingsResult.getStatus() == 2){
+            mConnectOrDisconnect.setText(R.string.disconnect);
+        } else {
+            mConnectOrDisconnect.setText(R.string.connect);
+        }
+        mWanSettingsParams.setConnectType(2);
+        mSelectedPppopImg.setVisibility(View.GONE);
+        mSelectedDhcpImg.setVisibility(View.GONE);
+        mSelectedStaticIpImg.setVisibility(View.VISIBLE);
+        mPppopLayout.setVisibility(View.GONE);
+        mDhcpTextview.setVisibility(View.GONE);
+        mStaticIpLayout.setVisibility(View.VISIBLE);
+    }
+
+    private void showConnectDhcp() {
+        if(mWanSettingsResult.getConnectType() == 1 && mWanSettingsResult.getStatus() == 2){
+            mConnectOrDisconnect.setText(R.string.disconnect);
+        } else {
+            mConnectOrDisconnect.setText(R.string.connect);
+        }
+        mWanSettingsParams.setConnectType(1);
+        mSelectedPppopImg.setVisibility(View.GONE);
+        mSelectedDhcpImg.setVisibility(View.VISIBLE);
+        mSelectedStaticIpImg.setVisibility(View.GONE);
+        mPppopLayout.setVisibility(View.GONE);
+        mDhcpTextview.setVisibility(View.VISIBLE);
+        mStaticIpLayout.setVisibility(View.GONE);
+    }
+
+    private void showConnectPppoe() {
+        if(mWanSettingsResult.getConnectType() == 0 && mWanSettingsResult.getStatus() == 2){
+            mConnectOrDisconnect.setText(R.string.disconnect);
+        } else {
+            mConnectOrDisconnect.setText(R.string.connect);
+        }
+        mWanSettingsParams.setConnectType(0);
+        mSelectedPppopImg.setVisibility(View.VISIBLE);
+        mSelectedDhcpImg.setVisibility(View.GONE);
+        mSelectedStaticIpImg.setVisibility(View.GONE);
+        mPppopLayout.setVisibility(View.VISIBLE);
+        mDhcpTextview.setVisibility(View.GONE);
+        mStaticIpLayout.setVisibility(View.GONE);
     }
 
     @Override
